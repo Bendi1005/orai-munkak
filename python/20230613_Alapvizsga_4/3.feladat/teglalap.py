fc2592cfcaa92d6239fcd6e835c9287725e14b42
Molnár Bendegúz <teafozo.yutube@gmail.com>
2023-06-13 10:17:40 +0200

    2023_06_13 munka

---
+++ python/20230613_Alapvizsga_4/3.feladat/teglalap.py
@@ -0,0 +1,9 @@
+print('1. feladat: Téglalap területe és kerülete')
+for i in range(10):
+    print(f'Adja meg a {i + 1} téglalap adatait!')
+    a = float(input('a = '))
+    b = float(input('b = '))
+    print(f'T = {a * b}')
+    print(f'K = {(a + b) * 2}')
+
+
